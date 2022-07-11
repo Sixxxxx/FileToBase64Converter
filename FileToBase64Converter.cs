2ccf3114b761fb5556f61afc174f63d6f2c0ce4f
six <jcuudeh@gmail.com>
2022-07-11 12:30:05 +0100

    initial commit

---
+++ FileToBase64Converter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace FileToBase64Converter
+{
+    public static class FileConverter
+    {
+        public static string ConvertToBase64(IFormFile file)
+        {
+            if (file.Length > 0)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    file.CopyTo(ms);
+                    var fileBytes = ms.ToArray();
+                    return Convert.ToBase64String(fileBytes);
+                }
+            }
+            return null;
+        }
+    }
+}
